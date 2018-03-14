a99e0b68a5ef1f1bd443e31328a4978f0d19192e
Lucie-Lgp <lucie.legoupil@gmail.com>
2018-03-14 13:56:46 +0200

    Add files via upload

---
+++ LUCIE!.py
@@ -0,0 +1,12 @@
+forename="Lucie"
+print(forename) 
+forename="Lucie"
+print(forename)
+surname="Legoupil"
+print(surname)
+forename="Lucie "
+surname="Petit"
+print(forename+surname)
+colour="brown"
+animal="dog"
+print(colour+ " " + animal)
